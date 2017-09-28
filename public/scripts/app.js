1457a2a6cc53ed5aaa0b0f6ce6109e305955f708
Deanna Lepke <deannalepke@gmail.com>
2017-09-28 21:25:04 +0000

    app.js can now produce an array of ordered options from /pollgit add public/scripts/app.js views/git add public/scripts/app.js views/

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,8 +3,9 @@ $(() => {
   $(".submitoptions").on("click", function(event) {
     event.preventDefault();
 
-    var orderArray = $("li").each(function(i) {
-      $(this).attr('id');
+    var orderArray = [];
+    $("li").each(function() {
+      orderArray.push($(this).attr("rel"));
     });
     console.log(orderArray);
 
