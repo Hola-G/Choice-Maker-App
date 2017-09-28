09017aaec7e3c93cc299f4bb0a0313693a1d959e
Chad Garrett <chadgarrett_@hotmail.com>
2017-09-28 15:09:04 -0700

    Added /test POST route

--- server.js
+++ server.js
@@ -2,38 +2,42 @@
 
 require('dotenv').config();
 
-const PORT        = process.env.PORT || 8080;
-const ENV         = process.env.ENV || "development";
-const express     = require("express");
-const bodyParser  = require("body-parser");
-const sass        = require("node-sass-middleware");
-const app         = express();
+const PORT            = process.env.PORT || 8080;
+const ENV             = process.env.ENV || "development";
+const express         = require("express");
+const bodyParser      = require("body-parser");
+const sass            = require("node-sass-middleware");
+const app             = express();
 
-const knexConfig  = require("./knexfile");
-const knex        = require("knex")(knexConfig[ENV]);
-const morgan      = require('morgan');
-const knexLogger  = require('knex-logger');
+const knexConfig      = require("./knexfile");
+const knex            = require("knex")(knexConfig[ENV]);
+const morgan          = require('morgan');
+const knexLogger      = require('knex-logger');
+const pollDataHelper  = require('./lib/pollDataHelpers.js')(knex);
 
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
 // 'dev' = Concise output colored by response status for development use.
 //         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
-app.use(morgan('dev'));
+//app.use(morgan('dev'));
 
 // Log knex SQL queries to STDOUT as well
-app.use(knexLogger(knex));
+//app.use(knexLogger(knex));
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/styles", sass({
   src: __dirname + "/styles",
   dest: __dirname + "/public/styles",
-  debug: true,
+  debug: false,
   outputStyle: 'expanded'
 }));
 app.use(express.static("public"));
 
 // Home page
 app.get("/", (req, res) => {
+  let pollData = pollDataHelper.getPollByID(1).then((results) => {
+    console.log(results[0].poll_title);
+  });
   res.render("index");
 });
 
@@ -60,6 +64,10 @@ app.get("/thankyou", (req, res) => {
 
 app.post("/poll", (req, res) => {
   console.log(req);
+})
+
+app.post("/test", (req, res) => {
+  console.log(req.body);
 })
 
 
