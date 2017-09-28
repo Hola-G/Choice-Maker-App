b6f9bf72c3b7523912412ea06bff7a92b529a82a
Deanna Lepke <deannalepke@gmail.com>
2017-09-28 21:11:02 +0000

    started getting post from /polls to work

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,5 +1,13 @@
 $(() => {
-  // Document Ready stuff here
 
+  $(".submitoptions").on("click", function(event) {
+    event.preventDefault();
+
+    var orderArray = $("li").each(function(i) {
+      $(this).attr('id');
+    });
+    console.log(orderArray);
+
+  });
 
 });
