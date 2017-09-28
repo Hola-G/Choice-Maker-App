c541d3f0210cb6cafbef7852695910847b7feda9
Jonny Kalambay <jonnyk20@gmail.com>
2017-09-28 19:58:59 +0000

    seeded votes

--- db/seeds/start_polls.js
+++ db/seeds/start_polls.js
@@ -1,8 +1,11 @@
 exports.seed = function(knex, Promise) {
     return Promise.all([
-        knex('options').insert({poll_id: 1, option_name: 'apple'}),
-        knex('options').insert({poll_id: 1, option_name: 'banana'}),
-        knex('options').insert({poll_id: 1, option_name: 'orange'})
+        knex('votes').insert({ option_id: 1, score: 5}),
+        knex('votes').insert({ option_id: 2, score: 2}),
+        knex('votes').insert({ option_id: 3, score: 4}),
+        knex('votes').insert({ option_id: 1, score: 9}),
+        knex('votes').insert({ option_id: 2, score: 1}),
+        knex('votes').insert({ option_id: 3, score: 5})
     ])
   };
   
