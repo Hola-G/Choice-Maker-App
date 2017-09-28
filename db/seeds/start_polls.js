c772378f4fa9032454f89d840492e9466987f2e7
Chad Garrett <chadgarrett_@hotmail.com>
2017-09-27 17:39:40 -0700

    Restructured seeds to clear tables prior to data entry

--- db/seeds/start_polls.js
+++ db/seeds/start_polls.js
@@ -1,24 +1,18 @@
 exports.seed = function(knex, Promise) {
-  return knex('polls').del()
-    .then(function () {
-      return Promise.all([
-
-        // poll
+    return Promise.all([
+        knex('polls').del(),
+        knex('options').del(),
+        knex('votes').del(),
         knex('polls').insert({ poll_title: 'fruit', email: 'food@food.com'}),
-
-        // 3 options
         knex('options').insert({poll_id: 1, option_name: 'apple'}),
         knex('options').insert({poll_id: 1, option_name: 'banana'}),
         knex('options').insert({poll_id: 1, option_name: 'orange'}),
-
-        // 2 submissions ( 6 votes )
         knex('votes').insert({ option_id: 1, score: 5}),
         knex('votes').insert({ option_id: 2, score: 2}),
         knex('votes').insert({ option_id: 3, score: 4}),
         knex('votes').insert({ option_id: 1, score: 9}),
         knex('votes').insert({ option_id: 2, score: 1}),
-        knex('votes').insert({ option_id: 3, score: 5}),
-
-      ]);
-    });
-};
+        knex('votes').insert({ option_id: 3, score: 5})
+    ])
+  };
+  
