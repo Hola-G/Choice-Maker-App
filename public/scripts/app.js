7e5c46a16dd8bdd26722e026e811a494fa47365d
Deanna Lepke <deannalepke@gmail.com>
2017-09-28 22:33:40 +0000

    started working on createpoll front end

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -8,7 +8,16 @@ $(() => {
       orderArray.push($(this).attr("rel"));
     });
     console.log(orderArray);
-
+    //orderArray now returns the order submitted!!!!!
+    //use rel not id in html
   });
+
+////////////////////////
+
+  $(".creatediv").on("click", function(event) {
+    event.preventDefault();
+    $("#makeoptions").append("<p><input name='option' placeholder='Enter option'></input>")
+  });
+
 
 });
