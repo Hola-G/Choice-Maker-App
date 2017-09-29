1118ef763e91abcac45078ec3e97d156321df3b2
Deanna Lepke <deannalepke@gmail.com>
2017-09-29 00:12:38 +0000

    /createpoll now sends user input to server

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -16,8 +16,47 @@ $(() => {
 
   $(".creatediv").on("click", function(event) {
     event.preventDefault();
-    $("#makeoptions").append("<p><input name='option' placeholder='Enter option'></input>")
+    $(".options_section").append(`<div class="option_container">
+      <p><input name="option_name" class="option_name" placeholder="Enter option"></input>
+      <p><input name="option_desc" class="option_desc" placeholder="Enter description"></input></p>
+    </div>`)
   });
 
+
+  $("#createpoll").on("click", function(event) {
+    event.preventDefault();
+
+    var optionNames = [];
+    var optionDescs = [];
+    var optionsObjectsArray = [];
+
+    $('.option_container').each(function() {
+      optionsObjectsArray.push({
+        option_name: $(this).find('.option_name').val(),
+        option_desc: $(this).find('.option_desc').val()
+      });
+    })
+
+
+    console.log(optionsObjectsArray);
+
+
+
+
+
+    var poll_title = $(".poll_title").val();
+    var email = $(".email").val();
+    var options = optionsObjectsArray;
+
+    $.ajax({
+      method: 'POST',
+      url: '/test',
+      data: {
+       poll_title: poll_title,
+       email: email,
+       options: options
+      }
+    })
+  })
 
 });
