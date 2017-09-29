341fec282ce93bfc560663c2e5235c72550d66ea
Chad Garrett <chadgarrett_@hotmail.com>
2017-09-29 09:00:21 -0700

    Moved routing to separate polls.js file

--- server.js
+++ server.js
@@ -13,7 +13,9 @@ const knexConfig      = require("./knexfile");
 const knex            = require("knex")(knexConfig[ENV]);
 const morgan          = require('morgan');
 const knexLogger      = require('knex-logger');
-const dataHelper  = require('./lib/data-helpers.js')(knex);
+
+// Seperated Routes for each Resource
+const pollsRoutes = require("./routes/polls");
 
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
 // 'dev' = Concise output colored by response status for development use.
@@ -33,69 +35,8 @@ app.use("/styles", sass({
 }));
 app.use(express.static("public"));
 
-// Home page
-app.get("/", (req, res) => {
-  dataHelper.getPollByID(1).then((results) => {
-    let pollTitle = results[0].poll_title;
-    console.log(pollTitle);
-    res.render("index", { pollTitle: pollTitle });
-  });
-});
-
-app.get("/createpoll", (req, res) => {
-  res.render("createpoll");
-});
-
-app.post("/createpoll", (req, res) => {
-  let poll_title = req.body.poll_title;
-  let email = req.body.email;
-  let options = req.body.options;
-
-  dataHelper.createPoll(poll_title, email).then((results) => {
-    dataHelper.createOptions(results[0], options).then((results) => {
-      res.send("Success!" + results);
-    })
-  })
-});
-
-app.get("/poll-successfully-created", (req, res) => {
-  res.render("poll-successfully-created");
-});
-
-app.get("/poll/:id", (req, res) => {
-  return Promise.all([dataHelper.getPollByID(req.params.id),dataHelper.getOptionsByPollID(req.params.id)])
-    .then((results) => {
-    let poll = { poll_title: results[0][0].poll_title, email: results[0][0].email, options: results[1] }
-    res.render("poll", { poll: poll });
-  });
-});
-
-app.get("/results/:id", (req, res) => {
-  let poll = { 
-    poll_title: 'Whats your favorite color?', 
-    email: 'chad@you.com', 
-    options: [
-      { option_name: 'Red', option_desc: 'I just love red!', score: 232 },
-      { option_name: 'Blue', option_desc: 'The sky is blue!', score: 132 },
-      { option_name: 'Green', option_desc: 'Grass isnt always greener!', score: 82 },
-      { option_name: 'Black', option_desc: 'Its a dark, dark world..!', score: 15 }
-    ] 
-  }
-  
-  res.render("results", { poll: poll });
-});
-
-app.get("/thankyou", (req, res) => {
-  res.render("thankyou");
-});
-
-
-app.post("/poll", (req, res) => {
-  dataHelper.submitVotes(req.body.options).then((results) => {
-    res.status(200).send();
-  });
-  // console.log(req);
-})
+// Mount all resource routes
+app.use("/", pollsRoutes(knex));
 
 app.listen(PORT, () => {
   console.log("Example app listening on port " + PORT);
