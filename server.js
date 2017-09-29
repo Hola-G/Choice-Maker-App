c9e28964d7ef3cbd8c90b31fd06152d7dbe69d75
Chad Garrett <chadgarrett_@hotmail.com>
2017-09-29 08:53:23 -0700

    Renamed, cleaned up functions old console.logs

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const knexConfig      = require("./knexfile");
 const knex            = require("knex")(knexConfig[ENV]);
 const morgan          = require('morgan');
 const knexLogger      = require('knex-logger');
-const pollDataHelper  = require('./lib/pollDataHelpers.js')(knex);
+const dataHelper  = require('./lib/data-helpers.js')(knex);
 
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
 // 'dev' = Concise output colored by response status for development use.
@@ -35,7 +35,7 @@ app.use(express.static("public"));
 
 // Home page
 app.get("/", (req, res) => {
-  pollDataHelper.getPollByID(1).then((results) => {
+  dataHelper.getPollByID(1).then((results) => {
     let pollTitle = results[0].poll_title;
     console.log(pollTitle);
     res.render("index", { pollTitle: pollTitle });
@@ -51,16 +51,11 @@ app.post("/createpoll", (req, res) => {
   let email = req.body.email;
   let options = req.body.options;
 
-  pollDataHelper.createPoll(poll_title, email).then((results) => {
-    console.log(results[0]);
-    pollDataHelper.createOptions(results[0], options).then((results) => {
-      console.log(results);
+  dataHelper.createPoll(poll_title, email).then((results) => {
+    dataHelper.createOptions(results[0], options).then((results) => {
       res.send("Success!" + results);
     })
   })
-
-  // console.log(req.body);
-  // res.send('You have posted!');
 });
 
 app.get("/poll-successfully-created", (req, res) => {
@@ -68,21 +63,26 @@ app.get("/poll-successfully-created", (req, res) => {
 });
 
 app.get("/poll/:id", (req, res) => {
-  let getPollData = Promise.all([
-    pollDataHelper.getPollByID(req.params.id),
-    pollDataHelper.getOptionsByPollID(req.params.id)
-  ]).then((results) => {
-    let poll = {
-      poll_title: results[0][0].poll_title,
-      email: results[0][0].email,
-      options: results[1]
-    }
+  return Promise.all([dataHelper.getPollByID(req.params.id),dataHelper.getOptionsByPollID(req.params.id)])
+    .then((results) => {
+    let poll = { poll_title: results[0][0].poll_title, email: results[0][0].email, options: results[1] }
     res.render("poll", { poll: poll });
   });
 });
 
-app.get("/results", (req, res) => {
-  res.render("results");
+app.get("/results/:id", (req, res) => {
+  let poll = { 
+    poll_title: 'Whats your favorite color?', 
+    email: 'chad@you.com', 
+    options: [
+      { option_name: 'Red', option_desc: 'I just love red!', score: 232 },
+      { option_name: 'Blue', option_desc: 'The sky is blue!', score: 132 },
+      { option_name: 'Green', option_desc: 'Grass isnt always greener!', score: 82 },
+      { option_name: 'Black', option_desc: 'Its a dark, dark world..!', score: 15 }
+    ] 
+  }
+  
+  res.render("results", { poll: poll });
 });
 
 app.get("/thankyou", (req, res) => {
@@ -91,32 +91,11 @@ app.get("/thankyou", (req, res) => {
 
 
 app.post("/poll", (req, res) => {
-  pollDataHelper.submitVotes(req.body.options).then((results) => {
+  dataHelper.submitVotes(req.body.options).then((results) => {
     res.status(200).send();
   });
   // console.log(req);
 })
-
-app.post("/test", (req, res) => {
-  console.log(req.body);
-  res.end();
-})
-
-app.get("/chad", (req, res) => {
-  let testArray = [
-    { option_name: 'Blue', option_desc: '' },
-    { option_name: 'red', option_desc: '' },
-    { option_name: 'yellow', option_desc: '' }
-  ]
-  pollDataHelper.createOptions(1, testArray).then((result) => {
-    console.log(result);
-    res.status(201);
-  }).catch((error) => {
-    res.status(400);
-  });
-  res.send();
-});
-
 
 app.listen(PORT, () => {
   console.log("Example app listening on port " + PORT);
